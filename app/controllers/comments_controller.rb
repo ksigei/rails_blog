fca57b371bf23ab4019a435ad09410061086b9e2
ksigei <sigeikiprono4@gmail.com>
2022-09-24 15:51:35 +0300

    Add views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,23 @@ class CommentsController < ApplicationController
     @comments = Comment.find(:all)
     @post = comments.post.find(:all)
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create(comments_params)
+    @comment.user = current_user
+    @comment.post = @post
+    if @comment.save
+      flash[:success] = 'Comment created!'
+      redirect_to user_post_path(current_user, @post)
+    else
+      flash.now[:error] = 'Something went wrong'
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'Post created!'
+      redirect_to user_posts_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
