2c0ead1921cc19204628ca9e6f9f5c686f419c1d
kip <kip@localhost.localdomain>
2022-10-01 14:52:45 +0300

    Ensure all tests are running

--- spec/controllers/_controller_spec.rb
+++ spec/controllers/_controller_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 require 'rails_helper'
 
-# RSpec.describe PostsController, type: :controller do
-#   describe 'GET #index' do
-#     it 'returns http success' do
-#       get :index
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
-
 RSpec.describe UsersController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
@@ -18,24 +9,6 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 end
-
-# RSpec.describe CommentsController, type: :controller do
-#   describe 'GET #comments' do
-#     it 'returns http success' do
-#       get :comments, params: { id: 1 }
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
-
-# RSpec.describe LikesController, type: :controller do
-#   describe 'GET #likes' do
-#     it 'returns http success' do
-#       get :likes, params: { id: 1 }
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
 
 RSpec.describe AboutController, type: :controller do
   describe 'GET #about' do
