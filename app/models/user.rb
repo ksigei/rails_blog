00564afa4d46a690d3bbec142d434da478c8628c
kip <kip@localhost.localdomain>
2022-10-01 21:38:15 +0300

    Solve rubo error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, presence: true
 
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
